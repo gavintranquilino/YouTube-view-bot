5738e32d693a21bde199236bd84cb01a6f04fa9c
Gavin Tranquilino <49102766+GuhBean@users.noreply.github.com>
2021-03-10 16:46:54 -0500

    Update webdriver manager

---
+++ driver.py
@@ -0,0 +1,37 @@
+from time import sleep
+from random import randint, choice
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.firefox import GeckoDriverManager
+
+class Bot(object):
+    """Handle connections"""
+
+    def __init__(self, website):
+        self.driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+        self.website = website  
+        self.wait = WebDriverWait(self.driver, 3)
+        self.presence = EC.presence_of_element_located
+        self.visible = EC.visibility_of_element_located
+
+    def get_vid(self):
+        self.driver.get(self.website)
+    
+    def play_video(self):
+        self.wait.until(self.visible((By.ID, "video-title")))
+        self.driver.find_element_by_xpath("//button[@class='ytp-large-play-button ytp-button']").click()
+
+    def clear_cache(self):
+        self.driver.delete_all_cookies()
+    
+    def refresh(self):
+        self.driver.implicitly_wait(5)
+        self.driver.refresh()
+    
+    def switch_tab(self, tab):
+        self.driver.switch_to.window(self.driver.window_handles[tab])
+    
+    def new_tab(self):
+        self.driver.execute_script("window.open('about:blank');")
--- main.py
+++ main.py
@@ -1,41 +1,5 @@
-from os import getcwd
 from json import load
-from time import sleep
-from random import randint, choice
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-class Bot(object):
-    """Handle connections"""
-
-    def __init__(self, website):
-        self.driver = webdriver.Firefox(executable_path=getcwd()+'\\geckodriver.exe')
-        self.website = website  
-        self.wait = WebDriverWait(self.driver, 3)
-        self.presence = EC.presence_of_element_located
-        self.visible = EC.visibility_of_element_located
-
-    def get_vid(self):
-        self.driver.get(self.website)
-    
-    def play_video(self):
-        self.wait.until(self.visible((By.ID, "video-title")))
-        self.driver.find_element_by_xpath("//button[@class='ytp-large-play-button ytp-button']").click()
-
-    def clear_cache(self):
-        self.driver.delete_all_cookies()
-    
-    def refresh(self):
-        self.driver.implicitly_wait(5)
-        self.driver.refresh()
-    
-    def switch_tab(self, tab):
-        self.driver.switch_to.window(self.driver.window_handles[tab])
-    
-    def new_tab(self):
-        self.driver.execute_script("window.open('about:blank');")
+import driver
 
 def get_config():
     with open('settings.json', 'r') as file:
@@ -68,7 +32,7 @@ def refresh_all(website, tab_amount):
 def main():
     print('Initilization')
     website, tab_amount, watch_time, view_cycles = get_config()
-    website = Bot(website)
+    website = driver.Bot(website)
 
     print('Opening new tabs')
     init_tabs(website, tab_amount)
