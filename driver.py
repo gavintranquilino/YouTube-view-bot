aebbd75ffc658be722317cc3dbbe24e2211e73c1
Gavin Tranquilino <49102766+GuhBean@users.noreply.github.com>
2021-03-12 16:53:21 -0500

    Add docstrings to functions

--- driver.py
+++ driver.py
@@ -10,7 +10,7 @@ from webdriver_manager.opera import OperaDriverManager
 from webdriver_manager.microsoft import IEDriverManager, EdgeChromiumDriverManager
 
 def get_driver(browser):
-    """Get the driver"""
+    """Get the webdriver specified in configuration"""
 
     # Browser name aliases
     chrome = ('chrome', 'google', 'google chrome', 'googlechrome', 'google-chrome', 'google_chrome')
@@ -43,6 +43,8 @@ class Bot(object):
     """Handle connections"""
 
     def __init__(self, website, browser):
+        """Initial function"""
+
         self.driver = get_driver(browser)
         self.website = website  
         self.wait = WebDriverWait(self.driver, 3)
@@ -50,21 +52,33 @@ class Bot(object):
         self.visible = EC.visibility_of_element_located
 
     def get_vid(self):
+        """Open the YouTube link"""
+
         self.driver.get(self.website)
     
     def play_video(self):
+        """Click on the play button"""
+
         self.wait.until(self.visible((By.ID, "video-title")))
         self.driver.find_element_by_xpath("//button[@class='ytp-large-play-button ytp-button']").click()
 
     def clear_cache(self):
+        """Clear the cache"""
+
         self.driver.delete_all_cookies()
     
     def refresh(self):
+        """Refresh the current tab"""
+
         self.driver.implicitly_wait(5)
         self.driver.refresh()
     
     def switch_tab(self, tab):
+        """Switch to the specified tab"""
+
         self.driver.switch_to.window(self.driver.window_handles[tab])
     
     def new_tab(self):
+        """Open a blank new tab"""
+        
         self.driver.execute_script("window.open('about:blank');")
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from time import sleep
 import driver
 
 def get_config():
+    """Read configuration file"""
+
     try:
         with open('default.json', 'r') as file:
             data = load(file)
@@ -11,57 +13,69 @@ def get_config():
         with open('config.json', 'r') as file:
             data = load(file)
     
-    return data["website"], data["tab_amount"], data["watch_time"], data["view_cycles"], data["browser"]
+    return data
 
 def init_tabs(website, tab_amount):
+    """Opens tabs according to tab amount set in config.json"""
+
     for _ in range(tab_amount):
         website.new_tab()
 
 def open_links(website, tab_amount):
+    """Open the YouTube link in each tab"""
+
     for tab in range(tab_amount):
         # Open links
         website.switch_tab(tab)
         website.get_vid()
 
 def play_video(website, tab_amount):
+    """Click on the play button"""
+
     for tab in range(tab_amount):
         # Play the video on each tab
         website.switch_tab(tab)
         website.play_video()
 
 def refresh_all(website, tab_amount):
+    """Refresh all tabs"""
+
     for tab in range(tab_amount):
         # Refresh all tabs
         website.switch_tab(tab)
         website.refresh()
 
 def main():
+    """Main Function"""
+
     print('Initilization')
-    website, tab_amount, watch_time, view_cycles, browser = get_config()
-    website = driver.Bot(website, browser)
+    config = get_config()
+
+    website = driver.Bot(config['website'], config['browser'])
 
     print('Opening new tabs')
-    init_tabs(website, tab_amount)
+    init_tabs(website, config['tab_amount'])
 
     print('Open links')
-    open_links(website, tab_amount)
+    open_links(website, config['tab_amount'])
     
     print('Cycle start')
     print('Playing videos')
-    play_video(website, tab_amount)
+    play_video(website, config['tab_amount'])
 
-    for i in range(view_cycles):
+    for i in range(config['view_cycles']):
+
         # Cycle the amount of refreshes
-        sleep(watch_time) # Watch the video for n amount of times
+        sleep(config['watch_time']) # Watch the video for n amount of times
 
         print('Refreshing all tabs')
-        refresh_all(website, tab_amount)
+        refresh_all(website, config['tab_amount'])
 
         print('Clearing cache')
         website.clear_cache() # Clear cache and site cookies
-        print(f"Run {i+1}/{view_cycles} complete")
+        print(f"Run {i+1}/{config['view_cycles']} complete")
     
-    print('Cycles complete')
+    print('Complete')
 
 if __name__ == '__main__':
     main()
